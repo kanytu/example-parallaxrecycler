015edb0bc24c635cb6055be6837db188832c6585
Daniel Smith <danoz73@gmail.com>
2015-06-11 11:12:44 -0700

    Fix project library import issue

--- app/src/main/java/com/poliveira/apps/exampleparallaxrecycler/MainActivity.java
+++ app/src/main/java/com/poliveira/apps/exampleparallaxrecycler/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.poliveira.parallaxrecyclerview.adapter.ParallaxRecyclerAdapter;
+import com.poliveira.parallaxrecycleradapter.ParallaxRecyclerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
